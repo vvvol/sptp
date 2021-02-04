4e1496e4c29f77b46803767205388eb0746b0405
AselSB <41651062+AselSB@users.noreply.github.com>
2021-02-04 17:37:12 +0300

    Second phase
    The fixation of some variables was added which is defined by special mask. For instance, on the second phase ETF and Equities are fixed with the values that were obtained in the first phase,whereas other variables being optimized.

---
+++ SptpData_6_2.py
@@ -0,0 +1,129 @@
+from math import *
+import pandas as pd
+
+
+class SptpData_6_2:
+    def __init__(self, name, path2a_csv="", path2b_csv="", path2C_csv="", path2D_csv="", path2P_csv="", path2S_csv="", path2T_csv="", path_sol="",  path_mask="", debug=False):
+        """
+         :param name: name of the dataset;
+         :path2a_csv: path to CSV with Available Assets (vector, i)
+         :path2b_csv: path to CSV with Minimal Requirements (vector, j)
+         :path2C_csv: path to CSV with Internal Costs of Lots (matrix, [ij])
+         :path2D_csv: path to CSV with maximum cost of requirement j that can be satisfied with assets of type k (matrix, [kj])
+         :path2P_csv: path to CSV with External costs of Lots (matrix, [ij])
+         :path2S_csv: path to CSV with Lots' Sizes
+         :path2T_csv: path to CSV with indicator of whether asset ih as type k (0 or 1).There is exactly one1 in each row (matrix [ik]
+         :xIntThreshold: SEE def isXinteger(self, i, j) !!!!
+        """
+        self.name = name
+
+        self.aVec = pd.read_csv(path2a_csv, index_col=0)
+        self.bVec = pd.read_csv(path2b_csv, index_col=0)
+        self.cMat = pd.read_csv(path2C_csv, index_col=0)
+        self.dMat = pd.read_csv(path2D_csv, index_col=0)
+        self.pMat = pd.read_csv(path2P_csv, index_col=0)
+        self.sMat = pd.read_csv(path2S_csv, index_col=0)
+        self.tMat = pd.read_csv(path2T_csv, index_col=0)
+        #self.initX = pd.read_csv(path_sol, index_col=0)
+        self.initX = pd.read_csv(path_sol, index_col=[0, 1], skipinitialspace=True)
+        self.mask = pd.read_csv(path_mask, index_col=0)
+        # self.xIntThreshold =xIntThreshold
+
+        self.I = self.aVec.index
+        self.M = len(self.I)
+
+        self.J = self.bVec.index
+        self.N = len(self.J)
+
+        self.K = self.dMat.index
+        self.NK = len(self.K)
+
+        # Check data integrity
+        iBuf = len(self.cMat.index)
+        if iBuf != self.M:
+            raise ValueError('M of rows in C [%d] != length of Assets list [%d]' % (iBuf, self.M) )
+        iBuf = len(self.cMat.columns.values.tolist())
+        if iBuf != self.N:
+            raise ValueError('N of columns in C [%d] != length of Requirements list [%d]' % (iBuf, self.N) )
+
+        iBuf = len(self.tMat.columns.values.tolist())
+        if iBuf != self.NK:
+            raise ValueError('K of rows in D [%d] != length of number Assets types [%d] (columns of T' % (iBuf, self.NK))
+        iBuf = len(self.dMat.columns.values.tolist())
+        if iBuf != self.N:
+            raise ValueError('N of columns in D [%d] != length of Requirements list [%d]' % (iBuf, self.N))
+
+        iBuf = len(self.pMat.index)
+        if iBuf != self.M:
+            raise ValueError('M of rows in P [%d] != length of Assets list [%d]' % (iBuf, self.M) )
+        iBuf = len(self.pMat.columns.values.tolist())
+        if iBuf != self.N:
+            raise ValueError('N of columns in P [%d] != length of Requirements list [%d]' % (iBuf, self.N) )
+
+        iBuf = len(self.sMat.index)
+        if iBuf != self.M:
+            raise ValueError('M of rows in S [%d] != length of Assets list [%d]' % (iBuf, self.M) )
+        iBuf = len(self.sMat.columns.values.tolist())
+        if iBuf != self.N:
+            raise ValueError('N of columns in S [%d] != length of Requirements list [%d]' % (iBuf, self.N) )
+        # ====================================
+
+    def getC(self, i, j):
+        return self.cMat.loc[i][j]
+
+    def getA(self, i):
+        return self.aVec.loc[i]['Units']
+
+    def getB(self, j):
+        return self.bVec.loc[j]['Amount']
+
+    def getS(self, i, j):
+        return self.sMat.loc[i][j]
+    def getX2A(self, i, j):
+        return self.getS(i, j) #self.sMat.loc[i][j]
+
+    def getP(self, i, j):
+        return self.pMat.loc[i][j]
+    def getX2B(self, i, j):
+        return self.getP(i, j) #self.pMat.loc[i][j]
+
+    def getD(self, k, j):
+        return self.dMat.loc[k][j]
+
+    def getT(self, i, k):
+        return self.tMat.loc[i][k]
+    
+    def getmask(self, i, j):
+        return self.mask.loc[i][j]
+        
+
+    def getInitX(self, i, j):
+#         if (i, j) in self.initX.index:
+#             return self.initX.loc[(i, j)].iloc[0]
+#         else:
+        return 0
+        
+    def getInitX2(self, i, j):
+        if (i, j) in self.initX.index:
+            return self.initX.loc[(i, j)].iloc[0]
+        else:
+            return 0
+            #return self.initX.loc[i][j]
+    
+    def checkFeasible(self, debug=False):
+        checkDict = {}
+        # for j in self.J:
+        #     if self.getB(j) > sum(self.getD(i,j) for i in self.I):
+        #         checkDict[j] = False
+        #         if debug:
+        #             print('checkFeasible: ', j, ' FALSE')
+        #     else:
+        #         checkDict[j] = True
+        return checkDict
+
+    # !!! All non-currency x_{ij} ARE INTEGER
+    def isXinteger(self, i, j):
+        if len(str(i)) > 3:
+            return True
+        else:
+            return False
---
+++ demoSptp_6_2.py
@@ -0,0 +1,222 @@
+from __future__ import print_function
+from future.utils import iteritems
+
+import os
+import sys
+import argparse
+import pandas as pd
+
+from write import write_nl_only
+from write import get_smap_var
+from read import read_sol_smap_var
+
+# import ssop_config
+from ssop_session import *
+
+import ssop_config
+from sptpmodel_6_2 import *
+from SptpData_6_2 import *
+
+def makeParser():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-pr', '--problem', default="dv4", help='problem name')
+    parser.add_argument('-a', '--action', type=str, default='nl', choices=['nl', 'sol'],
+                        help='Make NL or import SOL')
+    # parser.add_argument('-int', '--integer', action='store_true', help='variables are integer')
+    # parser.add_argument('-inthld', '--integerthreshold', type=int, default=0,
+    #                     help= 'Is X_ij Integer by its upper bound threshold, the more the value the more integers')
+    parser.add_argument('-wd', '--workdir', default=ssop_config.SSOP_DEFAULT_WORKING_DIR, help='working directory')
+    parser.add_argument('-s', '--solver', default='scip', choices=['ipopt', 'scip'], help='solver to use')
+    parser.add_argument('-cf', '--cleanfiles', action='store_true', help='clean working directory')
+    parser.add_argument('-cj', '--cleanjobs', action='store_true', help='clean jobs from server')
+    parser.add_argument('-x', '--extra', action='store_true', help='extra tests')
+    return parser
+
+def makeIpoptOptionsFile(workdir, optFileName):
+    # see https://coin-or.github.io/Ipopt/OPTIONS.html
+    with open(workdir + "/" + optFileName, 'w') as f:
+        f.write("linear_solver ma57\n")
+        f.write("max_iter 10000\n")
+        f.write("constr_viol_tol 0.0001\n")
+        f.write("warm_start_init_point yes\n")
+        f.write("warm_start_bound_push 1e-06\n")
+        f.write("print_level 4\n")
+        f.write("print_user_options yes\n")
+        f.close()
+    return
+
+def makeScipOptionsFile(workdir, optFileName):
+    # https://scip.zib.de/doc-6.0.2/html/PARAMETERS.php
+    with open(workdir + "/" + optFileName, 'w') as f:
+        f.write('display/freq = 100\n')
+        f.write('display/verblevel = 4\n')
+        f.write('limits/gap = 1e-06\n')
+        f.write('limits/memory = 28000\n')
+        f.close()
+    return
+
+def makeNlFile(theModel, workdir, **params):
+    # theModel = SPTPmodel(SptpData(M, N), isInteger=True)
+    nlName = write_nl_only(theModel.model, workdir + '/' + theModel.name,  symbolic_solver_labels=True)
+    return nlName
+
+def checkResults(theModel, workdir):
+    smap = get_smap_var(theModel.model)
+    results = read_sol_smap_var(theModel.model, workdir + "/" + theModel.name, smap)
+    theModel.model.solutions.load_from(results)
+    # solution have been loaded to the model
+    print("Solution of ", theModel.name)
+
+    with open(workdir + "/" + theModel.name + '.sol.txt', 'w') as f:
+        sBuf = ('Optimal cost: %.3f' % (theModel.model.obj()))
+        print(sBuf)
+        f.write(sBuf + '\n')
+
+        sBuf = 'i, j, x_ij (>1.e-6)'
+        print(sBuf)
+        f.write(sBuf + '\n')
+
+        for i in theModel.model.I:
+            for j in theModel.model.J:
+                if theModel.model.x[i,j]() > 1.e-6:
+                    sBuf = ("%s, %s, %.2f" % (str(i), str(j), theModel.model.x[i,j]()))
+                    print(sBuf)
+                    f.write(sBuf+'\n')
+        f.close()
+
+    return
+
+if __name__ == "__main__":
+    parser = makeParser()
+    args = parser.parse_args()
+    # vargs = vars(args)
+    print('Arguments of the test')
+    print('======================')
+    for arg in vars(args):
+        print(arg + ":", getattr(args, arg))
+    print('======================')
+
+    workdir = args.workdir
+    solver = args.solver
+
+    # Create opt/ model and write NL-file
+    # Get SptpData somehow
+    if args.action == "nl":
+        print('Reading data and feasibility checking started...')
+    if args.action == 'sol':
+        print('Reading data...')
+    start_read_check = timer()
+
+    theData = SptpData_6_2(args.problem, \
+                       path2a_csv='/home/sagingalieva/financial/sptp/data/a_vector.csv', \
+                       path2b_csv='/home/sagingalieva/financial/sptp/data/b_vector.csv', \
+                       path2C_csv='/home/sagingalieva/financial/sptp/data/c_matrix.csv', \
+                       path2D_csv='/home/sagingalieva/financial/sptp/data/d_matrix.csv', \
+                       path2P_csv='/home/sagingalieva/financial/sptp/data/p_matrix.csv', \
+                       path2S_csv='/home/sagingalieva/financial/sptp/data/s_matrix.csv', \
+                       path2T_csv='/home/sagingalieva/financial/sptp/data/t_matrix.csv', \
+                       path_sol='/home/sagingalieva/financial/sptp/data/sol.csv',\
+                       path_mask='/home/sagingalieva/financial/sptp/data/mask.csv')
+    # Check necessary condition for feasibility
+    if args.action == 'nl':
+        checkDict = theData.checkFeasible(debug=True)
+        for j in checkDict.keys():
+            if not checkDict[j]:
+                raise ValueError("Feasibility check failed for j = %s" % (str(j)))
+
+    if args.action == "nl":
+        print('Reading and feasibility checking took: %g sec' % (timer() - start_read_check))
+    if args.action == 'sol':
+        print('Reading took: %g sec' % (timer() - start_read_check))
+
+
+    print('Model creating ...')
+    start_model = timer()
+    theModel = SPTPmodel_6_2(theData, isInteger=True, debug=False)
+    # with open(workdir + '/' + theModel.name + '.mod.txt', 'w') as f:
+    #     theModel.model.pprint(ostream=f)
+    #     f.close()
+    print('Model creating took: %g sec' % (timer() - start_model))
+
+    # Write NL-files
+    if args.action == 'nl':
+        print('Writing NL-file')
+        start_makeNl = timer()
+        nlName = makeNlFile(theModel, workdir)
+        nlNames = [nlName]
+        stop_makeNl = timer()
+        print('makeNl(%s, version 4) took: %g sec' % (theModel.name, stop_makeNl - start_makeNl))
+
+        # # Write options file
+        # if solver == "scip":
+        #     optFile = 'scipdemo.set'
+        #     # makeScipOptionsFile(workdir, optFile)
+        #     makeSolverOptionsFile(workdir + "/" + optFile, solver="scip", \
+        #                           dictOptVal={"display/freq": 100, "display/verblevel": 5, \
+        #                                       "limits/gap": 1e-06, "limits/memory": 28000}
+        #                           )
+        # if solver == "ipopt":
+        #     optFile = 'ipopt.opt'
+        #     # makeIpoptOptionsFile(workdir, optFile)
+        #     makeSolverOptionsFile(workdir + "/" + optFile, solver="ipopt", \
+        #                           linear_solver="ma57", max_iter=10000, \
+        #                           constr_viol_tol=0.0001,
+        #                           warm_start_init_point="yes", warm_start_bound_push=1e-06, \
+        #                           print_level=4, print_user_options="yes")
+        # quit()
+
+    # Write SOL-files
+    if args.action == 'sol':
+        print('Checking SOL-file')
+        checkResults(theModel, workdir)
+        quit()
+
+    quit()
+
+
+    # resources_list = [ssop_config.SSOP_RESOURCES["test-pool-scip-ipopt"]] #['']] #["vvvolhome2"]] # ["ui4.kiae.vvvol"]] 'hse'
+    # theSession = SsopSession(name=args.problem, resources=resources_list, \
+    #                          workdir=workdir, debug=False)
+    # # theSession = SsopSession(name=args.problem, resources=[ssop_config.SSOP_RESOURCES["ui4.kiae.vvvol"]], \
+    # #                          workdir=workdir, debug=False)
+    #
+    # # Variables declarations for Python 3.*
+    # optFile = ""
+    # solved = []
+    # unsolved = []
+    # jobId = ""
+    #
+    #
+    # # Solve all problems by SSOP
+    # if solver == "ipopt":
+    #     solved, unsolved, jobId = theSession.runJob(nlNames, optFile) # by default solver = "ipopt"
+    # if solver == "scip":
+    #     solved, unsolved, jobId = theSession.runJob(nlNames, optFile, solver="scip")
+    #
+    # print("solved:   ", solved)
+    # print("unsolved: ", unsolved)
+    # print("Job %s is finished" % (jobId))
+    #
+    # # Check results
+    # checkResults(theModel, workdir)
+    #
+    #
+    # # Clean working directory to free disk space at local host, MAY BE
+    # if args.cleanfiles:
+    #     theSession.deleteWorkFiles([".nl", ".row", ".col", ".sol", ".zip", ".plan"])
+    #
+    # # Delete jobs created to save disk space at Everest server , MAY BE
+    # if args.cleanjobs:
+    #     theSession.deleteAllJobs()
+    #
+    # # CLOSE THE SESSION !!! MUST BE
+    # theSession.session.close()
+    #
+    # print("Done")
+
+
+
+
+
+
+
